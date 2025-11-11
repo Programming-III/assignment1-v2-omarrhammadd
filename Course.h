d509020dd7c331cb73ef7aad1ea941344024c117
omarrhammadd <omarahmadkamalhammad@gmail.com>
2025-11-11 09:56:36 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -3,8 +3,24 @@
 
 #include <string>
 #include "Student.h"
+#include <iostream>
 using namespace std;
-//#write your code here
+class Course {
+private:
+	string courseCode;
+	string courseName;
+	int maxStudents;
+	Student* students;
+	int currentStudents;
+
+public:
+	Course(string coursCode, string CourseName, int maxStudents);
+	Course();
+	void addStudent(const Student& s);
+	void displayCourseInfo();
+
+
+};
 
 
 
